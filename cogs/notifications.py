e8432fe9e440d0da084b2ff3a80124f32ef7b01b
Nimendra Kariyawasam <57085116+nimendrak@users.noreply.github.com>
2022-09-01 16:58:07 +0000

    minor changes to notifications cog

--- cogs/notifications.py
+++ cogs/notifications.py
@@ -31,13 +31,15 @@ class Notifications(commands.Cog, name="Notifications Cog"):
     async def notify(
         self,
         interaction: Interaction,
-        link: str,
+        link: str = SlashOption(
+            description="Link to the published content",
+        ),
         platform: str = SlashOption(
             name="picker",
+            description="Select the type your content",
             choices={"Tiktok": "tt", "Youtube Short": "yts", "Youtube Video": "ytv", "TikTok Live": "ttl"},
         ),
     ):
-        mention_everyone = nextcord.AllowedMentions(everyone = True)
         announcement_channel = self.client.get_channel(GUILD_DATA.get("ANNOUNCEMENTS_CHANNEL"))
         content_updates_channel = self.client.get_channel(GUILD_DATA.get("CONTENT_UPDATES_CHANNEL"))
 
@@ -45,40 +47,36 @@ class Notifications(commands.Cog, name="Notifications Cog"):
             if platform == "tt":
                 await content_updates_channel.send(
                     content = f"""
-                    @ everyone
+                    @everyone
                     > BOT Lanthe posted a new clip on TikTok. Make sure you check it out!. **Link:** {link}
                     """,
-                    allow_mentions = mention_everyone
                 )
             elif platform == "yts":
                 await content_updates_channel.send(
                     content = f"""
-                    @ everyone
+                    @everyone
                     > Bot Lanthe just uploaded a Youtube Short. Make sure you check it out!. **Link:** {link}
                     """,
-                    allow_mentions = mention_everyone
                 )
             elif platform == "ytv":
                 await content_updates_channel.send(
                     content = f"""
-                    @ everyone
+                    @everyone
                     > Bot Lanthe uploaded a full Youtube video. Make sure you check it out!. **Link:** {link}
                     """,
-                    allow_mentions = mention_everyone
                 )
             elif platform == "ttl":
                 await announcement_channel.send(
                     content = f"""
-                    @ everyone
+                    @everyone
                     > We're streaming live on TikTok, follow this link to check it out. **Link:** {link}
                     """,
-                    allow_mentions = mention_everyone
                 )
-            await interaction.response.send_message(f"Notification has been sent.", ephemeral=False)
+            await interaction.response.send_message(f"{interaction.user.mention} sent a notification by @ {time.strftime('%a, %d %b %Y %H:%M:%S')}.", ephemeral=False)
             log.warning(f"{interaction.user} sent a notification to | {content_updates_channel}") 
         else:
             await interaction.response.send_message(f"You don't have the required permissions to use this command.", ephemeral=False)
-            log.warning(f"User tried to use a notification command | { interaction.user.roles}") 
+            log.warning(f"User tried to use a notification command | { interaction.user.mention}") 
 
 def setup(client):
     client.add_cog(Notifications(client))
