e40d5df46ae8aea42713663d6f0dee7646cc964e
Nimendra Kariyawasam <57085116+nimendrak@users.noreply.github.com>
2022-09-05 12:06:45 +0000

    Divided yt and tt notifications to two different channels

--- cogs/notifications.py
+++ cogs/notifications.py
@@ -1,20 +1,18 @@
-from random import choices
 from helpers.logger_config import logger as log
 from helpers.project_config import ProjectConfigManager
 from helpers.bot_config import BotConfigManager
-import logging
 import nextcord
 from nextcord import Interaction, SlashOption
 from nextcord.ext import commands
 import time
+from datetime import datetime
+from dateutil.tz import gettz
 
 # Get project config instance
 pcm = ProjectConfigManager()
 
 BOT = pcm.get_bots_config()
 GUILD_DATA = pcm.get_guild_data()
-
-log = logging.getLogger(__name__)
         
 class Notifications(commands.Cog, name="Notifications Cog"):
     def __init__(self, client):
@@ -37,34 +35,40 @@ class Notifications(commands.Cog, name="Notifications Cog"):
         platform: str = SlashOption(
             name="picker",
             description="Select the type your content",
-            choices={"Tiktok": "tt", "Youtube Short": "yts", "Youtube Video": "ytv", "TikTok Live": "ttl"},
+            choices={"TikTok Post": "tt", "Youtube Short": "yts", "Youtube Video": "ytv", "TikTok Live": "ttl"},
         ),
     ):
         announcement_channel = self.client.get_channel(GUILD_DATA.get("ANNOUNCEMENTS_CHANNEL"))
-        content_updates_channel = self.client.get_channel(GUILD_DATA.get("CONTENT_UPDATES_CHANNEL"))
+        yt_updates_channel = self.client.get_channel(GUILD_DATA.get("YT_UPDATES_CHANNEL"))      
+        tt_updates_channel = self.client.get_channel(GUILD_DATA.get("TT_UPDATES_CHANNEL"))
+
+        current_time = datetime.now(gettz("Asia/Colombo")).strftime("%a, %d %b %Y %H:%M:%S")
 
         if GUILD_DATA.get("AUTH_ROLES_ID")[0] in [role.id for role in interaction.user.roles]:
             if platform == "tt":
-                await content_updates_channel.send(
+                await tt_updates_channel.send(
                     content = f"""
                     @everyone
                     > BOT Lanthe posted a new clip on TikTok. Make sure you check it out!. **Link:** {link}
                     """,
                 )
+                await interaction.response.send_message(f"{interaction.user.mention} sent a **TikTok Post** notification @ {current_time}", ephemeral=False)
             elif platform == "yts":
-                await content_updates_channel.send(
+                await yt_updates_channel.send(
                     content = f"""
                     @everyone
                     > Bot Lanthe just uploaded a Youtube Short. Make sure you check it out!. **Link:** {link}
                     """,
                 )
+                await interaction.response.send_message(f"{interaction.user.mention} sent a **Youtube Short** notification @ {current_time}", ephemeral=False)
             elif platform == "ytv":
-                await content_updates_channel.send(
+                await yt_updates_channel.send(
                     content = f"""
                     @everyone
                     > Bot Lanthe uploaded a full Youtube video. Make sure you check it out!. **Link:** {link}
                     """,
                 )
+                await interaction.response.send_message(f"{interaction.user.mention} sent a **Youtube Video** notification @ {current_time}", ephemeral=False)
             elif platform == "ttl":
                 await announcement_channel.send(
                     content = f"""
@@ -72,10 +76,10 @@ class Notifications(commands.Cog, name="Notifications Cog"):
                     > We're streaming live on TikTok, follow this link to check it out. **Link:** {link}
                     """,
                 )
-            await interaction.response.send_message(f"{interaction.user.mention} sent a notification @ {time.strftime('%a, %d %b %Y %H:%M')}.", ephemeral=False)
-            log.warning(f"{interaction.user} sent a notification to | {content_updates_channel}") 
+                await interaction.response.send_message(f"{interaction.user.mention} sent a **TikTok Live** notification @ {current_time}", ephemeral=False)
+            log.warning(f"{interaction.user} sent a notification") 
         else:
-            await interaction.response.send_message(f"You don't have the required permissions to use this command.", ephemeral=False)
+            await interaction.response.send_message("You don't have the required permissions to use this command.", ephemeral=False)
             log.warning(f"User tried to use a notification command | { interaction.user.mention}") 
 
 def setup(client):
